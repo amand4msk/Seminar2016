209ce897b5b1ada07a16ca830ea2045fdc41d476
Maria Schmidt <maria_schmidt91@yahoo.de>
2016-03-22 21:50:40 +0100

    Maria

--- polls/static/polls/fb_functions.js
+++ polls/static/polls/fb_functions.js
@@ -2,7 +2,35 @@
  * Script
  */
 
+function getCookie(name) {
+var cookieValue = null;
+if (document.cookie && document.cookie != '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+        }
+    }
+}
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+console.log(csrftoken);
 
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
 
 var accessTokeb = 'CAACEdEose0cBAC1n8ZC9XS4DJtNU79CZBsMFURg66aBhO4vHb71pJpUM6T901lSDcMe5A9xTTj027Pz8dd3Ixarqrx3w116LCH5lAPQ3kW8mTO8qALqO9uAyZCyDmZAfNlBC9E5uMg2sLYX8JpydzY8sWrBIs7IOeFO8ZC8YDxiyGaxslqizL8T6JT8D4kPPGNHrNZA25FKQZDZD';
 // Only works after `FB.init` is called
@@ -33,10 +61,10 @@ function procBatch(dat) { // handle this batch, request the next batch
 function procRow(dat)
 {
 	console.log(dat)
-  /*  $.post('/polls/like_category/', dat, function(response){
+    $.post('/polls/like_category/', dat, function(response){
         if(response == 'success') { alert('Yay!');}
         else{alert('dump');}
-    });*/
+    });
    
 }
 	
