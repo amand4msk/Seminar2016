d798fbb7aae5d5644723db84b68533c7baf27f3c
Maria Schmidt <maria_schmidt91@yahoo.de>
2016-03-23 10:32:28 +0100

    test

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-from .models import Person
+from .models import Person, Post
 
 import logging
 
