dde530b35f3cc5a8e5d8a5073ce80fabdbf4e438
Amanda Karavolia <amand4msk@gmail.com>
2016-03-23 14:31:24 +0100

    add comment blabla

--- seminar2016/urls.py
+++ seminar2016/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
+    #blabla
     # url(r'^$', 'seminar2016.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^polls/', include('polls.urls')),
