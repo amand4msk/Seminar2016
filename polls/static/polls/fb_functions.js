aa46053c9138bea3b2485800b5171b1ad678c24d
Maria Schmidt <maria_schmidt91@yahoo.de>
2016-03-31 13:28:12 +0200

    likes

--- polls/static/polls/fb_functions.js
+++ polls/static/polls/fb_functions.js
@@ -63,7 +63,7 @@ function myFacebookLogin() {
         
 
 function getMueller() {   // calls the first batch of records
-	   FB.api("/es.muellert.wieder/posts",{},function(response) { console.log(response) } );
+	   FB.api("/es.muellert.wieder/objects",{},function(response) { console.log(response) } );
 }
 	
 function procBatch(dat) { // handle this batch, request the next batch
